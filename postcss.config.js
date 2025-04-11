4cc98c5aaf92d73842a4bd7e2b315d70c1d29763
EnemyofSauron666 <EnemyofSauron666@gmail.com>
2025-04-10 19:07:25 -0700

    Fix PostCSS config to use new Tailwind plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
-  
+xmodule.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
+}
