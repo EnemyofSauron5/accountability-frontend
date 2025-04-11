5ebb36ac780f9f52237f35fc056af00a1ae5d24d
EnemyofSauron666 <EnemyofSauron666@gmail.com>
2025-04-11 10:29:21 -0700

    Fix Cloudflare redirect rule to enable React routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-white text-black flex flex-col items-center">
-        <Header />
-        <main className="w-full max-w-4xl px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </div>
+      <Header />
+      <main className="...">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
-
-export default App;
