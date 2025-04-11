bfc419e7ee6bca330ef626772409c6f56b2a0648
EnemyofSauron666 <EnemyofSauron666@gmail.com>
2025-04-11 09:17:31 -0700

    Reintegrate Tailwind CSS with proper config and imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // or './App.jsx' if you don’t rename
+import './index.css';
 import './styles/main.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
